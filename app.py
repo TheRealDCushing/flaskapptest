cc7db261e97309d105eaba5c1488bdaed170785b
Priyarag <47515549+Priyarag@users.noreply.github.com>
2019-06-29 22:35:58 -0700

    Update app.py

--- app.py
+++ app.py
@@ -126,6 +126,7 @@ def sqllivedata():
       row["price"] = result[1]
       row["crytodatetime"] = result[1]
       live_totals.append(row)
+      print(live_totals)
   # Return a JSON list of Temperature Observations (tobs) for the previous year.
     return(json.dumps(live_totals, default=alchemyencoder))
 #     items = [dict(r) for r in results]
