ce145642c83c51688c5ca5f54924af89a85f511a
Priyarag <47515549+Priyarag@users.noreply.github.com>
2019-06-30 12:40:05 -0700

    Update app.py

--- app.py
+++ app.py
@@ -144,7 +144,7 @@ def sqllivedata():
 #     return(json.dumps({'items': livedata},default=alchemyencoder))
 @app.route("/livedata/<userSelectedCrypto1>/<userSelectedCrypto2>/<userSelectedDateTime1>")
 #/<firstCurrency>&<secondCurrency>&<dateTimeInput1>&<dateTimeInput2>")
-def collect_data_bycurrency_datetime(userSelectedCrypto1=None,userSelectedCrypto2=None,userSelectedDateTime1=None,userSelectedDateTime2=None):
+def collect_data_bycurrency_datetime(userSelectedCrypto1=None,userSelectedCrypto2=None,userSelectedDateTime1=None):
     results = session.query(Crypto_Table.symbol, func.max(Crypto_Table.price),func.date_format(Crypto_Table.crypto_timestamp,"%Y-%m-%d %h:%i:00"))\
       .filter(Crypto_Table.symbol.in_([userSelectedCrypto1,userSelectedCrypto2])).filter(cast(Crypto_Table.crypto_timestamp, DateTime) >= userSelectedDateTime1)\
       .group_by(Crypto_Table.symbol,func.date_format(Crypto_Table.crypto_timestamp,"%Y-%m-%d %h:%i:00"))\
