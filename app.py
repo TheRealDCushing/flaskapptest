1411bfe0ed0d2b748819801ce39d6836fc4c5576
Priyarag <47515549+Priyarag@users.noreply.github.com>
2019-06-29 21:43:08 -0700

    Update app.py

--- app.py
+++ app.py
@@ -116,7 +116,7 @@ def stations():
     return(json.dumps({'items': items}, default=alchemyencoder))
 @app.route("/api/v1.0/livedata")
 def sqllivedata():
-    results = session.query(Crypto_Table.symbol, func.max(Crypto_Table.price),func.date_format(Crypto_Table.crypto_timestamp,"%Y-%m-%d %h:%i:00"))\
+    results = session.query(Crypto_Table.symbol, func.max(Crypto_Table.price).label("price"),cast(func.date_format(Crypto_Table.crypto_timestamp,"%Y-%m-%d %h:%i:00"),String).label("cryptodatetime"))\
      .group_by(Crypto_Table.symbol,func.date_format(Crypto_Table.crypto_timestamp,"%Y-%m-%d %h:%i:00"))\
     .all()
     # filter(cast(Crypto_Table.crypto_timestamp, Timestamp) <= dateTimeInput2).distinct().all()
